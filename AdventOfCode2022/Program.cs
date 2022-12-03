23237685c660c8c02d21e793b4374c581c1ca57e
Jakub <kuborgc6@gmail.com>
2022-12-03 17:34:02 +0100

    Division items per compartments

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Day 3!");
 string textInput = System.IO.File.ReadAllText(@"C:\Users\SiDzej\source\repos\AdventOfCode2022\AdventOfCode2022\inputfile.txt");
 char c = 'a';
+string[] rucksacks = textInput.Split('\n');
+
 int index = 0;
 if (Char.IsUpper(c))
 {
@@ -11,3 +13,14 @@ else
     index = char.ToUpper(c) - 64;
 }
 Console.WriteLine(index);
+
+foreach (string rucksack in rucksacks)
+{
+    string compartmentOne = rucksack.Substring(0, rucksack.Length / 2);
+    string compartmentTwo = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
+    Console.WriteLine(rucksack);
+    Console.WriteLine(compartmentOne);
+    Console.WriteLine(compartmentTwo);
+    break;
+
+}
