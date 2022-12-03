204dec71ec8ab0cdbd8a5d2395ab00f017e93ea2
Jakub <kuborgc6@gmail.com>
2022-12-03 17:25:48 +0100

    First version of getting priorities of items in rucksacks.

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,1 +1,13 @@
 ﻿Console.WriteLine("Day 3!");
+string textInput = System.IO.File.ReadAllText(@"C:\Users\SiDzej\source\repos\AdventOfCode2022\AdventOfCode2022\inputfile.txt");
+char c = 'a';
+int index = 0;
+if (Char.IsUpper(c))
+{
+    index = char.ToUpper(c) - 64 + 26;
+}
+else
+{
+    index = char.ToUpper(c) - 64;
+}
+Console.WriteLine(index);
