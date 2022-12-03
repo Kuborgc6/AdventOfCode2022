447ac682391e04802eb6d42b52528c2f68072d02
Jakub <kuborgc6@gmail.com>
2022-12-03 16:57:14 +0100

    Solution of second half of day 2

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Day 2");
+﻿Console.WriteLine("Day 2");
 string textInput = System.IO.File.ReadAllText(@"C:\Users\SiDzej\source\repos\AdventOfCode2022\AdventOfCode2022\inputfile.txt");
 string[] games = textInput.Split('\n');
 int score = 0;
@@ -32,7 +30,41 @@ foreach (var game in games)
             break;
     }
 }
-Console.WriteLine(score);
-//
+Console.WriteLine("Score without instructions: " + score);
 
-//Console.WriteLine(game[1].Remove(1) == "Z");
+//-------
+score = 0;
+
+foreach (var game in games)
+{
+    string[] players = game.Split(' ');
+    players[1] = players[1].Remove(1);
+    switch (players[1])
+    {
+        case "Y":
+            score += 3;
+            break;
+        case "Z":
+            score += 6;
+            break;
+    }
+    switch ((players[0], players[1]))
+    {
+        case ("A", "X"):
+        case ("B", "Z"):
+        case ("C", "Y"):// 3 
+            score += 3;
+            break;
+        case ("A", "Y"):
+        case ("B", "X"):
+        case ("C", "Z"):// 1
+            score += 1;
+            break;
+        case ("A", "Z"):
+        case ("B", "Y"):
+        case ("C", "X"):
+            score += 2; // 2
+            break;
+    }
+}
+Console.WriteLine("Score with instructions: " + score);
