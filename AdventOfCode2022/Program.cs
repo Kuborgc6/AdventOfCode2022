28b9855d0e3df7c93a81a58a8897617ea908b2dd
Jakub <kuborgc6@gmail.com>
2022-12-04 22:38:02 +0100

    Solution of first half of Day 3

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,26 +1,27 @@
 ﻿Console.WriteLine("Day 3!");
-string textInput = System.IO.File.ReadAllText(@"C:\Users\SiDzej\source\repos\AdventOfCode2022\AdventOfCode2022\inputfile.txt");
-char c = 'a';
+//string textInput = File.ReadAllText("./inputfile.txt");
+string textInput = File.ReadAllText(@"C:\Users\SiDzej\source\repos\AdventOfCode2022\AdventOfCode2022\inputfile.txt");
 string[] rucksacks = textInput.Split('\n');
 
-int index = 0;
-if (Char.IsUpper(c))
-{
-    index = char.ToUpper(c) - 64 + 26;
-}
-else
-{
-    index = char.ToUpper(c) - 64;
-}
-Console.WriteLine(index);
-
+int allpriorities = 0;
 foreach (string rucksack in rucksacks)
 {
     string compartmentOne = rucksack.Substring(0, rucksack.Length / 2);
     string compartmentTwo = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
-    Console.WriteLine(rucksack);
-    Console.WriteLine(compartmentOne);
-    Console.WriteLine(compartmentTwo);
-    break;
-
+    string isManyTimes = "";
+    foreach (char item in compartmentOne)
+    {
+        int priority = 0;
+        if ((compartmentTwo.Contains(item)) && !(isManyTimes.Contains(item)))
+        {
+            isManyTimes += item;
+            if (Char.IsUpper(item))
+                priority = item - 64 + 26;
+            else
+                priority = item - 96;
+            allpriorities += priority;
+        }
+    }
 }
+
+Console.WriteLine("The sum of the priorities of items misplaced in rucksacks: " + allpriorities);
