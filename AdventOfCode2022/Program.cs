a21f0fd14c7e23a470567ab015f3a977481abead
Kuborgc6 <kuborgc6@gmail.com>
2022-12-05 19:58:47 +0100

    Changing containers in to list of strings.

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,35 @@
-﻿Console.WriteLine("Hello Advent of Code 2022!");
+﻿Console.WriteLine("Day 5!");
 
 string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
 string sFilePath = Path.GetFullPath(sFile);
 string textInput = @File.ReadAllText(sFilePath);
+
+string[] devision = textInput.Split("\r\n\r\n");
+Console.WriteLine(devision[0]);
+//Console.WriteLine(devision[1]);
+string[] containersLines = devision[0].Split("\n");
+Console.WriteLine(containersLines[2][1+4]);
+
+// number of containers = length/4
+int containersNumber = containersLines[0].Length / 4;
+Console.WriteLine(containersNumber);
+
+string[] containers = new string[containersNumber];
+foreach (string containerLine in containersLines)
+{
+    for (int i = 0; i < containersNumber; i++)
+    {
+        containers[i] += containerLine[1 + i * 4];
+    }
+}
+
+for (int i = 0; i < containersNumber; i++)
+{
+    char[] charTemp = containers[i].ToCharArray();
+    Array.Reverse(charTemp);
+    containers[i] = new string(charTemp);
+}
+
+Console.WriteLine(containers[1]);
+Console.WriteLine(containers[1].Length);
