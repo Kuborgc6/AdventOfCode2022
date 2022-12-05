20e365caca860c7c1988b5ac34df322b1deb248c
Jakub <kuborgc6@gmail.com>
2022-12-05 12:12:24 +0100

    Solution of first half of day 4

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,8 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Day 4!");
+﻿Console.WriteLine("Day 4!");
 
 string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
 string sFilePath = Path.GetFullPath(sFile);
-
 string textInput = @File.ReadAllText(sFilePath);
+
+string[] workPair = textInput.Split('\n');
+int howManyTimes = 0;
+foreach (string workers in workPair)
+{
+    string[] assignments = workers.Split(',');
+    if(assignments[1].Length > 3)
+        assignments[1] = assignments[1].Remove(assignments[1].Length - 1); //Removes whitespace character
+    List<int> firstRange = assignments[0].Split('-').Select(int.Parse).ToList();
+    List<int> secondRange = assignments[1].Split('-').Select(int.Parse).ToList();
+    if (firstRange[0] <= secondRange[0] && firstRange[1] >= secondRange[1])
+        howManyTimes++;
+    else if (firstRange[0] >= secondRange[0] && firstRange[1] <= secondRange[1])
+        howManyTimes++;
+}
+
+Console.WriteLine("Assignment pairs that one range fully contain the other: " + howManyTimes);
