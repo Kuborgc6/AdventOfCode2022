6ee3d3b1a51f86b1f9968bf04cedd6b29e5d184d
Kuborgc6 <kuborgc6@gmail.com>
2022-12-06 11:49:25 +0100

    Solution of whole day 6!

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -4,3 +4,25 @@ string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
 string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
 string sFilePath = Path.GetFullPath(sFile);
 string textInput = @File.ReadAllText(sFilePath);
+
+string marker = "";
+for (int i = 0; i < textInput.Length - 4; i++)
+{
+    marker = textInput.Substring(i, 4);
+    if (marker.Distinct().Count() == marker.Length)
+    { 
+        Console.WriteLine("Marker is detected after " + (i + 4) + " characters processed");
+        break;
+    }
+}
+
+string message = "";
+for (int i = 0; i < textInput.Length - 14; i++)
+{
+    message = textInput.Substring(i, 14);
+    if (message.Distinct().Count() == message.Length)
+    {
+        Console.WriteLine("Message is detected after " + (i + 14) + " characters processed");
+        break;
+    }
+}
