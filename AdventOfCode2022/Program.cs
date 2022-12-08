17012ae1cdf407f9b7ec9f68f2bf0e9a6d866269
Kuborgc6 <kuborgc6@gmail.com>
2022-12-08 18:50:30 +0100

    First version of working on objects

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,36 @@
-﻿Console.WriteLine("Hello Advent of Code 2022!");
+﻿public class FolderDirectory
+{
+    public string currentFolder = "";
+    public List<FolderDirectory> subdirectories = new List<FolderDirectory>();
+    public List<string> files = new List<string>();
+}
 
-string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
-string sFilePath = Path.GetFullPath(sFile);
-string textInput = @File.ReadAllText(sFilePath);
+
+class Program
+{ 
+    static void Main()
+    { 
+        Console.WriteLine("Hello Advent of Code 2022!");
+
+        string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
+        string sFilePath = Path.GetFullPath(sFile);
+        string[] textInput = @File.ReadAllLines(sFilePath);
+
+        for (int i = 0; i < textInput.Length; i++)
+            textInput[i] = textInput[i].Trim();
+
+        FolderDirectory mainFolder = new FolderDirectory();
+
+        foreach (string line in textInput)
+        {
+            string[] splitLine = line.Split(' ');
+            if (splitLine[0] == "$" && splitLine[1] == "cd")
+            {
+                mainFolder.currentFolder = splitLine[2];
+                Console.WriteLine(line);
+            }
+        }       
+    }
+}
+
