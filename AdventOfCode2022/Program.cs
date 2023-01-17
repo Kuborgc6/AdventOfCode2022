c63338d18f1f7fb6ad73bf36aa4b09c38d073659
Kuborgc6 <kuborgc6@gmail.com>
2023-01-17 21:47:13 +0100

    First version of solution with building folder tree and recuring counting of folder size

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,10 +1,47 @@
-﻿public class FolderDirectory
-{
-    public string currentFolder = "";
-    public List<FolderDirectory> subdirectories = new List<FolderDirectory>();
-    public List<string> files = new List<string>();
-}
+﻿using System.IO;
 
+public class folderDirectory
+{
+    public string currentFolder { get; set; } = "";
+    public List<folderDirectory> subdirectories = new List<folderDirectory>();
+    public int size = 0;
+    public int correctSize = 0;
+
+    public (int,int) calculateChildrenSize()
+    {
+        int result = 0;
+        int howMany = 0;
+        if (this.subdirectories.Count > 0) 
+        {
+            Console.WriteLine("Debug 0: "+result);
+            foreach (folderDirectory tempFolder in this.subdirectories)
+            {
+                Console.WriteLine("Debug 1: " + tempFolder.currentFolder);
+                (int resultTemp, int howManyTemp) = tempFolder.calculateChildrenSize();
+                result += resultTemp;
+                howMany += howManyTemp;
+            }
+        }
+        else
+        {
+            result = this.size;
+        }
+        this.size = result;
+
+        if (this.size > 100000)
+            howMany += 1;
+
+        this.correctSize = howMany;
+
+        return (result, howMany);
+    }
+
+    public bool Equals(folderDirectory other)
+    {
+        if (other == null) return false;
+        return (this.currentFolder.Equals(other.currentFolder));
+    }
+}
 
 class Program
 { 
@@ -20,17 +57,59 @@ class Program
         for (int i = 0; i < textInput.Length; i++)
             textInput[i] = textInput[i].Trim();
 
-        FolderDirectory mainFolder = new FolderDirectory();
+        folderDirectory mainFolder = new folderDirectory();
+        folderDirectory tempFolder = mainFolder;
+        List<string> directory = new List<string>();
 
         foreach (string line in textInput)
         {
             string[] splitLine = line.Split(' ');
-            if (splitLine[0] == "$" && splitLine[1] == "cd")
+            if (line == "$ cd /")
             {
                 mainFolder.currentFolder = splitLine[2];
+                tempFolder = mainFolder;
                 Console.WriteLine(line);
             }
-        }       
+            else if (line == "$ cd ..")
+            {
+                directory.RemoveAt(directory.Count - 1);
+                tempFolder = mainFolder;
+                foreach (string oneDirectory in directory)
+                {
+                    tempFolder = tempFolder.subdirectories.Find(x => x.currentFolder == oneDirectory);
+                }
+                Console.WriteLine(line);
+            }
+            else if (splitLine[0] == "$" && splitLine[1] == "cd" )
+            {
+                Console.WriteLine(tempFolder.currentFolder);
+                directory.Add(splitLine[2]);
+                tempFolder = tempFolder.subdirectories.Find(x => x.currentFolder == splitLine[2]);
+                Console.WriteLine(tempFolder.currentFolder);
+
+                Console.WriteLine(line);
+            }
+            else if (line == "$ ls")
+            {
+                Console.WriteLine(line);
+            }
+            else if (splitLine[0] != "dir" && splitLine[0] != "$")
+            {
+                tempFolder.size += int.Parse(splitLine[0]);
+            }
+            else if (splitLine[0] == "dir")
+            {
+                folderDirectory subfolder = new folderDirectory();
+                subfolder.currentFolder = splitLine[1];
+                tempFolder.subdirectories.Add(subfolder);
+            }
+        }
+        (int resultSize, int resultFolders) = mainFolder.calculateChildrenSize();
+        Console.WriteLine("Result Size: " + resultSize);
+        Console.WriteLine("Result Folders: " + resultFolders);
+        Console.WriteLine("Children: ");
+        foreach (folderDirectory temp in mainFolder.subdirectories)
+            Console.WriteLine(temp.currentFolder);
     }
 }
 
