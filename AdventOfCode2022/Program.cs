8574db47fde09d78e1f70c10a8361aa4d5cf4007
Kuborgc6 <kuborgc6@gmail.com>
2023-01-18 22:44:14 +0100

    Solution of first half of day 8!

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,15 +1,95 @@
 ﻿using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
+using System.Reflection.Emit;
+using System.Xml.Linq;
 
 public class TreeCount
 {
     public int height = 0;
     public bool isSeen = false;
-
 }
+
+
 
 class Program
 {
+    public int visibleTrees(TreeCount[,] forest)
+    {
+        int result = 0;
+        int rowsOrHeight = forest.GetLength(0);
+        int colsOrWidth = forest.GetLength(1);
+
+        //from up left corner from left to right
+        for (int i = 0; i < rowsOrHeight; i++)
+        {
+            int highestTree = -1;
+            for (int j = 0; j < colsOrWidth; j++)
+            {
+                if (forest[i,j].height > highestTree)
+                {
+                    highestTree = forest[i, j].height;
+                    forest[i, j].isSeen = true;
+                }
+            }
+        }
+
+        //from up left corner from right to left
+        for (int i = 0; i < rowsOrHeight; i++)
+        {
+            int highestTree = -1;
+            for (int j = colsOrWidth - 1; j >= 0; j--)
+            {
+                if (forest[i,j].height > highestTree)
+                {
+                    highestTree = forest[i, j].height;
+                    forest[i, j].isSeen = true;
+                }
+            }
+        }
+
+        //from up left corner from up to down
+        for (int j = 0; j < colsOrWidth; j++)
+        {
+            int highestTree = -1;
+            for (int i = 0; i < rowsOrHeight; i++)
+            {
+                if (forest[i, j].height > highestTree)
+                {
+                    highestTree = forest[i, j].height;
+                    forest[i, j].isSeen = true;
+                }
+            }
+        }
+
+        //from up left corner from up to down
+        for (int j = 0; j < colsOrWidth; j++)
+        {
+            int highestTree = -1;
+            for (int i = rowsOrHeight - 1; i >= 0; i--)
+            {
+                if (forest[i, j].height > highestTree)
+                {
+                    highestTree = forest[i, j].height;
+                    forest[i, j].isSeen = true;
+                }
+            }
+        }
+
+        //counting result
+        for (int i = 0; i < rowsOrHeight; i++)
+        {
+            for (int j = 0; j < colsOrWidth; j++)
+            {
+                if (forest[i, j].isSeen == true)
+                {
+                    result++;
+                }
+            }
+        }
+        return result;
+    }
+
     static void Main()
     {
         Console.WriteLine("Hello Advent of Code 2022!");
@@ -36,8 +116,6 @@ class Program
             k++;
         }
 
-        foreach (string line in textInput)
-            Console.WriteLine(line);
 
         TreeCount[,] forest = new TreeCount[textInput.Length, textInput.Length];
         for (int i = 0; i < textInput.Length; i++)
@@ -46,9 +124,10 @@ class Program
                 TreeCount tempTree = new TreeCount();
                 tempTree.height = arrayInput[i, j];
                 forest[i, j] = tempTree;
-                
             }
 
-        Console.WriteLine(forest);
+        var p = new Program();
+        int result = p.visibleTrees(forest);
+        Console.WriteLine("All visible trees: " + result);
     }
 }
