de10edaee9e7805837aed6b2a261268db6750c24
Kuborgc6 <kuborgc6@gmail.com>
2023-01-18 19:14:37 +0100

    Created forest

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,7 @@
-﻿class TreeCount
+﻿using System.Collections.Generic;
+using System.IO;
+
+public class TreeCount
 {
     public int height = 0;
     public bool isSeen = false;
@@ -19,15 +22,33 @@ class Program
         for (int i = 0; i < textInput.Length; i++)
             textInput[i] = textInput[i].Trim();
 
+        int[,] arrayInput = new int[textInput.Length, textInput.Length];
+        int k = 0;
+        int l = 0;
+        foreach (string line in textInput)
+        {
+            l = 0;
+            foreach (char c in line)
+            {
+                arrayInput[k, l] = int.Parse(new string(c, 1));
+                l++;
+            }
+            k++;
+        }
+
         foreach (string line in textInput)
             Console.WriteLine(line);
 
         TreeCount[,] forest = new TreeCount[textInput.Length, textInput.Length];
         for (int i = 0; i < textInput.Length; i++)
             for (int j = 0; j < textInput.Length; j++)
+            { 
                 TreeCount tempTree = new TreeCount();
+                tempTree.height = arrayInput[i, j];
+                forest[i, j] = tempTree;
                 
-                forest[i,j] = 
+            }
 
+        Console.WriteLine(forest);
     }
 }
