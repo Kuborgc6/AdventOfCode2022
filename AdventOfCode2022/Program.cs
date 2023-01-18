9d26148ebf3006a7d714635827be854861a5ef24
Kuborgc6 <kuborgc6@gmail.com>
2023-01-18 19:03:11 +0100

    Added TreeCount class.

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,8 +1,33 @@
-﻿Console.WriteLine("Hello Advent of Code 2022!");
+﻿class TreeCount
+{
+    public int height = 0;
+    public bool isSeen = false;
 
-string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
-string sFilePath = Path.GetFullPath(sFile);
-string textInput = @File.ReadAllText(sFilePath);
+}
 
-Console.WriteLine(textInput);
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello Advent of Code 2022!");
+
+        string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
+        string sFilePath = Path.GetFullPath(sFile);
+        string[] textInput = @File.ReadAllLines(sFilePath);
+
+        for (int i = 0; i < textInput.Length; i++)
+            textInput[i] = textInput[i].Trim();
+
+        foreach (string line in textInput)
+            Console.WriteLine(line);
+
+        TreeCount[,] forest = new TreeCount[textInput.Length, textInput.Length];
+        for (int i = 0; i < textInput.Length; i++)
+            for (int j = 0; j < textInput.Length; j++)
+                TreeCount tempTree = new TreeCount();
+                
+                forest[i,j] = 
+
+    }
+}
