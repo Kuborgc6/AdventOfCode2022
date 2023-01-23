ace559e8e03bf581e2c1c96ec4cb45f6bbc7426d
Kuborgc6 <kuborgc6@gmail.com>
2023-01-23 18:02:46 +0100

    Edditing main file for easier work on

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,6 +1,15 @@
-﻿Console.WriteLine("Hello Advent of Code 2022!");
+﻿class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello Advent of Code 2022!");
 
-string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
-string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
-string sFilePath = Path.GetFullPath(sFile);
-string textInput = @File.ReadAllText(sFilePath);
+        string sCurrentDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
+        string sFilePath = Path.GetFullPath(sFile);
+        string[] textInput = @File.ReadAllLines(sFilePath);
+
+        for (int i = 0; i < textInput.Length; i++)
+            textInput[i] = textInput[i].Trim();
+    }
+}
