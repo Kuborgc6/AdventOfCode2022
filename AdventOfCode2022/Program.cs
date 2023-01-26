d10bfee9ae3f9080903547ff0876fd1519659e93
Kuborgc6 <kuborgc6@gmail.com>
2023-01-26 16:50:24 +0100

    Working on loading data

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,45 @@
-﻿class Program
+﻿class Monkey
+{
+    int ID;
+    List<int> startingItems;
+    int operation;
+    int test;
+    int trueMonkey;
+    int falseMonkey;
+    char functionMonkey;
+    int howMany = 0;
+
+    List<Monkey> chooseMonkeyList(List<Monkey> monkeyList)
+    {
+        foreach (int item in this.startingItems)
+        {
+            int chooseMonkey = -1;
+            int worrylevel = 0;
+            if (this.functionMonkey == '+')
+                worrylevel = item + this.operation;
+            else if (this.functionMonkey == '*')
+                worrylevel = item * this.operation;
+
+            int itemAfter = decimal.ToInt32(Math.Round(Convert.ToDecimal(worrylevel) / 3));
+            if (itemAfter % test == 0)
+                chooseMonkey = this.trueMonkey;
+            else
+                chooseMonkey = this.falseMonkey;
+            foreach (Monkey monkey in monkeyList)
+            {
+                if (monkey.ID == chooseMonkey)
+                    monkey.startingItems.Add(itemAfter);
+            }
+            howMany++;
+        }
+        this.startingItems.Clear();
+        return monkeyList;
+    }
+}
+
+
+
+class Program
 {
     static void Main()
     {
@@ -11,5 +52,15 @@
 
         for (int i = 0; i < textInput.Length; i++)
             textInput[i] = textInput[i].Trim();
+        
+        for (int i = 0; i < (textInput.Count()+1)/7; i++)
+        {
+            int IDMonkey = Convert.ToInt32(Char.GetNumericValue(textInput[i*7].Split(' ')[1][0]));
+            Console.WriteLine(textInput[i * 7 + 1].Split(':')[1].Trim().Split(", ")[0]);
+            Console.WriteLine(textInput[i * 7 + 2].Split("old")[1].Trim());
+        }
+        
+
+
     }
 }
