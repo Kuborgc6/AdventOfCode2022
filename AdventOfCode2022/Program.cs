86416566a66c45903ca76a43ad14e012d51ec700
Kuborgc6 <kuborgc6@gmail.com>
2023-01-26 13:05:42 +0100

    Solution of whole day 10!

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,4 +1,6 @@
-﻿class Program
+﻿using System.Linq;
+
+class Program
 {
     static void Main()
     {
@@ -12,8 +14,15 @@
         for (int i = 0; i < textInput.Length; i++)
             textInput[i] = textInput[i].Trim();
         int cycle = 0;
+        int cycleWrite = 0;
         int x = 1;
         int result = 0;
+        List<int> sprite = new List<int> {1, 2 ,3};
+
+        var empty_character = "░░";
+        var filled_character = "██";
+        string writtenText = "";
+
         foreach (string line in textInput)
         {
             string[] comands = line.Split(' ');
@@ -22,18 +31,54 @@
                 cycle++;
                 if (((cycle - 20) % 40 == 0) && (cycle <= 220))
                     result += x * cycle;
+
+                cycleWrite++;
+                if (cycleWrite > 40)
+                    cycleWrite -= 40;
+
+                if (sprite.Contains(cycleWrite))
+                    writtenText += filled_character;
+                else
+                    writtenText += empty_character;
             }
             else
             {
+                //First cycle
                 cycle++;
                 if (((cycle - 20) % 40 == 0) && (cycle <= 220))
                     result += x * cycle;
+                
+                cycleWrite++;
+                if (cycleWrite > 40)
+                    cycleWrite -= 40;
+
+                if (sprite.Contains(cycleWrite))
+                    writtenText += filled_character;
+                else
+                    writtenText += empty_character;
+                
+                //Second cycle
                 cycle++;
                 if (((cycle - 20) % 40 == 0) && (cycle <= 220))
                     result += x * cycle;
+
+                cycleWrite++;
+                if (cycleWrite > 40)
+                    cycleWrite -= 40;
+
+                if (sprite.Contains(cycleWrite))
+                    writtenText += filled_character;
+                else
+                    writtenText += empty_character;
+
                 x += int.Parse(comands[1]);
+                sprite[0] += int.Parse(comands[1]);
+                sprite[1] += int.Parse(comands[1]);
+                sprite[2] += int.Parse(comands[1]);
             }
         }
+        for (int i = 0; i < (writtenText.Count() / 80); i++)
+            Console.WriteLine(writtenText.Substring(i * 80,80));
 
         Console.WriteLine("Sum of six signals: " + result);
     }
