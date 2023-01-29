58c9f9209a0bab44aec13e2fe7c1b8c1b618001b
Kuborgc6 <kuborgc6@gmail.com>
2023-01-29 01:23:14 +0100

    Fixing mistakes in commits

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,45 +1,4 @@
-﻿class Monkey
-{
-    int ID;
-    List<int> startingItems;
-    int operation;
-    int test;
-    int trueMonkey;
-    int falseMonkey;
-    char functionMonkey;
-    int howMany = 0;
-
-    List<Monkey> chooseMonkeyList(List<Monkey> monkeyList)
-    {
-        foreach (int item in this.startingItems)
-        {
-            int chooseMonkey = -1;
-            int worrylevel = 0;
-            if (this.functionMonkey == '+')
-                worrylevel = item + this.operation;
-            else if (this.functionMonkey == '*')
-                worrylevel = item * this.operation;
-
-            int itemAfter = decimal.ToInt32(Math.Round(Convert.ToDecimal(worrylevel) / 3));
-            if (itemAfter % test == 0)
-                chooseMonkey = this.trueMonkey;
-            else
-                chooseMonkey = this.falseMonkey;
-            foreach (Monkey monkey in monkeyList)
-            {
-                if (monkey.ID == chooseMonkey)
-                    monkey.startingItems.Add(itemAfter);
-            }
-            howMany++;
-        }
-        this.startingItems.Clear();
-        return monkeyList;
-    }
-}
-
-
-
-class Program
+﻿class Program
 {
     static void Main()
     {
@@ -51,16 +10,10 @@ class Program
         string[] textInput = @File.ReadAllLines(sFilePath);
 
         for (int i = 0; i < textInput.Length; i++)
-            textInput[i] = textInput[i].Trim();
-        
-        for (int i = 0; i < (textInput.Count()+1)/7; i++)
         {
-            int IDMonkey = Convert.ToInt32(Char.GetNumericValue(textInput[i*7].Split(' ')[1][0]));
-            Console.WriteLine(textInput[i * 7 + 1].Split(':')[1].Trim().Split(", ")[0]);
-            Console.WriteLine(textInput[i * 7 + 2].Split("old")[1].Trim());
+            textInput[i] = textInput[i].Trim();
+            Console.WriteLine(textInput[i]);
         }
-        
-
 
     }
 }
