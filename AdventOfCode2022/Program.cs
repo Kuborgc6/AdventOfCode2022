8924d84c6276dfad64f2f00d4cdfc9d166e7a05b
Kuborgc6 <kuborgc6@gmail.com>
2023-02-12 17:13:59 +0100

    Change from literal values to numeric in input file

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -8,12 +8,32 @@
         string sFile = System.IO.Path.Combine(sCurrentDirectory, @"..\..\..\inputfile.txt");
         string sFilePath = Path.GetFullPath(sFile);
         string[] textInput = @File.ReadAllLines(sFilePath);
-
+        
         for (int i = 0; i < textInput.Length; i++)
         {
             textInput[i] = textInput[i].Trim();
-            Console.WriteLine(textInput[i]);
         }
 
+        int[,] map = new int[textInput.Length, textInput[0].Length];
+        for (int i = 0; i < textInput.Length; i++)
+        {
+            for(int j = 0; j < textInput[i].Length; j++)
+            {
+                if (textInput[i][j] == 'S')
+                    map[i, j] = 0;
+                else if (textInput[i][j] == 'E')
+                    map[i, j] = 27;
+                else
+                    map[i, j] = textInput[i][j] - 96;
+            }
+        }
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j]+"\t");
+            }
+            Console.Write("\n");
+        }
     }
 }
