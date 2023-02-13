065004ce2ac2a1bd8e032e7ebaffc7f6b24aefe1
Kuborgc6 <kuborgc6@gmail.com>
2023-02-13 15:21:35 +0100

    Copied version of DIjsktra

--- AdventOfCode2022/Program.cs
+++ AdventOfCode2022/Program.cs
@@ -1,5 +1,83 @@
 ﻿class Program
 {
+    // A utility function to find the
+    // vertex with minimum distance
+    // value, from the set of vertices
+    // not yet included in shortest
+    // path tree
+    static int V = 9;
+    int minDistance(int[] dist, bool[] sptSet)
+    {
+        // Initialize min value
+        int min = int.MaxValue, min_index = -1;
+
+        for (int v = 0; v < V; v++)
+            if (sptSet[v] == false && dist[v] <= min)
+            {
+                min = dist[v];
+                min_index = v;
+            }
+
+        return min_index;
+    }
+    public int dijsktra(int[,] map, int[] starCoord, int[] endCoord, int[,] graph, int src)
+    {
+        int result = 0;
+        int[] dist = new int[V]; // The output array. dist[i]
+                                 // will hold the shortest
+                                 // distance from src to i
+
+        // sptSet[i] will true if vertex
+        // i is included in shortest path
+        // tree or shortest distance from
+        // src to i is finalized
+        bool[] sptSet = new bool[V];
+
+        // Initialize all distances as
+        // INFINITE and stpSet[] as false
+        for (int i = 0; i < V; i++)
+        {
+            dist[i] = int.MaxValue;
+            sptSet[i] = false;
+        }
+
+        // Distance of source vertex
+        // from itself is always 0
+        dist[src] = 0;
+
+        // Find shortest path for all vertices
+        for (int count = 0; count < V - 1; count++)
+        {
+            // Pick the minimum distance vertex
+            // from the set of vertices not yet
+            // processed. u is always equal to
+            // src in first iteration.
+            int u = minDistance(dist, sptSet);
+
+            // Mark the picked vertex as processed
+            sptSet[u] = true;
+
+            // Update dist value of the adjacent
+            // vertices of the picked vertex.
+            for (int v = 0; v < V; v++)
+
+                // Update dist[v] only if is not in
+                // sptSet, there is an edge from u
+                // to v, and total weight of path
+                // from src to v through u is smaller
+                // than current value of dist[v]
+                if (!sptSet[v] && graph[u, v] != 0 && dist[u] != int.MaxValue && dist[u] + graph[u, v] < dist[v])
+                    dist[v] = dist[u] + graph[u, v];
+        }
+        return result;
+    }
+
+    public int myDijsktra(int[,] map, int[] starCoord, int[] endCoord)
+    {
+        int result = 0;
+
+        return result;
+    }
     static void Main()
     {
         Console.WriteLine("Hello Advent of Code 2022!");
